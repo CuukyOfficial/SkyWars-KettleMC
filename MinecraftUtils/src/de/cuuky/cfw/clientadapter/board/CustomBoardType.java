9083e8696a4852edf0460e9432d5fb87c91d815f
CuukyOfficial <just.cookie.jc@gmail.com>
2020-04-19 13:03:16 +0200

    Dunnoooooo

--- MinecraftUtils/src/de/cuuky/cfw/clientadapter/board/CustomBoardType.java
+++
@@ -1,9 +0,0 @@
-package de.cuuky.cfw.clientadapter.board;
-
-public enum CustomBoardType {
-
-	NAMETAG,
-	SCOREBOARD,
-	TABLIST;
-
-}
--- MinecraftUtils/src/de/cuuky/cfw/hooking/hooks/HookEntityType.java
+++
@@ -1,7 +0,0 @@
-package de.cuuky.cfw.hooking.hooks;
-
-public enum HookEntityType {
-
-	CHAT, ITEM;
-	
-}
